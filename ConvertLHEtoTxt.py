fa1f84a0dc27da00d12a5199a348380e61e1f44e
SkaKid420 <60874312+SkaKid420@users.noreply.github.com>
2020-04-23 09:20:39 +0200

    Add files via upload
    First set of files

---
+++ ConvertLHEtoTxt.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 10:08:46 2020
+
+@author: gerha
+"""
+import os
+import csv
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import tkinter as tk
+from tkinter import filedialog as fd
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+#print("Files in %r: %s" % (cwd, files))
+
+def ExtractInfo(String):
+    String.strip()
+    String = String.split()
+    ID = String[0]          #   PDG code                                        
+    IST = String[1]         #   status code
+    MOTH1 = String[2]       #   row number corresponding to the first mother particle 
+    MOTH2 = String[3]       #   row number corresponding to the second mother particle
+    ICOL1 = String[4]       #   first color tag  
+    ICOL2 = String[5]       #   second color tag
+    P1 = String[6]          #   PX 
+    P2 = String[7]          #   PY
+    P3 = String[8]          #   PZ  
+    P4 = String[9]          #   E         
+    P5 = String[10]         #   M (a space-like virtuality is denoted by a negative mass)
+    VTIM = String[11]       #   c tau 
+    SPIN = String[12]       #   cosine of the angle between the spin vector of the particle and its three-momentum  
+    return([ID, IST, MOTH1, MOTH2, ICOL1, ICOL2, P1, P2, P3, P4, P5, VTIM, SPIN]) 
+    
+def ValueP_T(P1, P2):
+    return np.sqrt(P1**2+P2**2)
+
+def ValueEta(P1, P2, P3):
+    CosTheta = P3/np.sqrt(P1**2  + P2**2 + P3**2)
+    return -0.5*np.log((1-CosTheta)/(1+CosTheta))
+
+def CreateFile(Folder_selected):
+    NewFile = open(Folder_selected + r'\LHEEventData.csv','w+', newline='')
+    writer = csv.writer(NewFile)
+    writer.writerow(["EventID","PDGID","IST","MOTH1","MOTH2","ICOL1","ICOL2","P1","P2","P3","P4","P5","VTIM","SPIN"])
+    EventID = 1
+    for root, dirs, files in os.walk(Folder_selected):
+      for name in files:
+         if "unweighted_events.lhe" in os.path.join(root, name):
+             if "unweighted_events.lhe.gz" not in os.path.join(root, name):
+                 #print(os.path.join(root, name))
+                 NewEventID , WList = ConverttoText(os.path.join(root, name),EventID)
+                 EventID = NewEventID
+                 print(EventID)
+                 for item in WList:
+                     writer.writerow(item)
+    print(EventID)                 
+    NewFile.close()
+
+
+def ConverttoText(LHEFile, EventID):
+    File = open(LHEFile, 'r')
+    LineText = File.readlines()
+    del LineText[0:LineText.index("</init>\n") +1 ]
+    WriteList = []
+    LocEventID = EventID
+    pbar = tqdm(total = len(LineText), leave = None)
+    while len(LineText) !=1 :
+        try:
+            for x in LineText[LineText.index('<event>\n')+2:LineText.index('<mgrwt>\n')]:
+                WriteList.append([LocEventID] + ExtractInfo(x))
+        except:
+            for x in LineText[LineText.index('<event>\n')+2:LineText.index('</event>\n')]:
+                WriteList.append([LocEventID] + ExtractInfo(x))
+        pbar.update(LineText.index('</event>\n') +1 - LineText.index('<event>\n'))
+        del LineText[LineText.index('<event>\n'):LineText.index('</event>\n') +1] 
+        LocEventID = LocEventID + 1   
+            
+    pbar.leave = None
+    pbar.update(len(LineText))   
+    pbar.close()   
+    File.close()
+    return [LocEventID, WriteList]
+
+
+def ConvertoPseudorapidity(SelectedDirectory):
+    DataSet = pd.read_csv(SelectedDirectory + r'\LHEEventData.csv' )
+    print(DataSet.head())
+    Temp = np.zeros((len(DataSet),2))
+    for i in tqdm(range(len(DataSet)), leave = None):
+        Temp[i,0] = ValueP_T(DataSet.P1[i], DataSet.P2[i])
+        Temp[i,1] = ValueEta(DataSet.P1[i],DataSet.P2[i], DataSet.P3[i])
+    DataSet.insert(loc = len(DataSet.columns), column = "DER_P_T", value = Temp[:,0])
+    DataSet.insert(loc= len(DataSet.columns), column = "DER_Eta", value = Temp[:,1])    
+    DataSet.to_csv(SelectedDirectory + r"\PsuedoRapidityDataSet.csv", index = False)
+    print(DataSet.head())   
+    
+    
+def recombineEvents(SelectedDirectory):
+    PDGID_Lepton_List = [11, 12, 13, 14, 15, 16, 17, 18]
+    PDGID_Boson_List =  [1, 2, 3, 4, 5, 6, 7, 8]
+    DataSet = pd.read_csv(SelectedDirectory + r"\PsuedoRapidityDataSet.csv")
+    EventDataSet = {"EventID" : [],
+                    "DER_No_Detected_Particles": [],
+                    "DER_No_unique_Particles":[],   
+                    "DER_No_Leptons":[],
+                    "DER_No_Bosons":[],
+                    "DER_PT_Tot_Detected_Particles":[],
+                    "DER_Delta_eta":[],
+                    "DER_Momentum_of_detected_Bosons":[],
+                    "DER_Momentum_of_detected_Leptons":[],
+                    "Label_Unique_Mother_Particles":[]}
+        
+    for i in tqdm(np.unique(DataSet.EventID), leave = None):   
+        Temp = DataSet[DataSet.EventID == i]
+        EventDataSet["EventID"].append(i)
+        EventDataSet["DER_No_Detected_Particles"].append(len(Temp[Temp.IST ==1 ]))
+        EventDataSet["DER_No_unique_Particles"].append(len(np.unique(Temp[Temp.IST == 1])))
+        EventDataSet["DER_No_Leptons"].append(len(Temp[Temp.IST == 1 & abs(Temp.PDGID).isin(PDGID_Lepton_List) ]))
+        EventDataSet["DER_No_Bosons"].append(len(Temp[Temp.IST == 1 & abs(Temp.PDGID).isin(PDGID_Boson_List) ]))
+        EventDataSet["DER_PT_Tot_Detected_Particles"].append(sum(Temp.DER_P_T[Temp.IST == 1]))
+        EventDataSet["DER_Delta_eta"].append(Temp.DER_Eta[Temp.IST == 1].iloc[0]  - Temp.DER_Eta[Temp.IST == 1].iloc[1])
+        EventDataSet["DER_Momentum_of_detected_Bosons"].append(sum(Temp.DER_P_T[Temp.IST == 1 & abs(Temp.PDGID).isin([PDGID_Boson_List])]))
+        EventDataSet["DER_Momentum_of_detected_Leptons"].append(sum(Temp.DER_P_T[Temp.IST == 1 & abs(Temp.PDGID).isin([PDGID_Lepton_List])]))
+        EventDataSet["Label_Unique_Mother_Particles"].append(np.unique(Temp.PDGID[Temp.IST == -1])[0])
+       
+    
+    df = pd.DataFrame(EventDataSet)
+    print("Sample of the data.")
+    print(df.head())
+    df.to_csv(SelectedDirectory + "\EventData.csv", index = False)    
+    
+def RunAllconversions():
+    SelectedDirectory = fd.askdirectory() 
+    #folder_selected = fd.askdirectory(title = "Location of the run folders", prompt = "Selected the folder containing the run files.")
+    i = 0 
+    for root, dirs, files in os.walk(SelectedDirectory):
+        for name in files:
+            if "unweighted_events.lhe" in os.path.join(root, name):
+              if "unweighted_events.lhe.gz" not in os.path.join(root, name):   
+                 i = i + 1
+    assert(i != 0 ), "No data files. Ensure that you have selected a folder containing the files 'unweighted_events.lhe'."
+    print("Found {} data unweighted event files.".format(i))
+    print("Converting txt file to csv format.")
+    CreateFile(SelectedDirectory)  
+    print("Adding the derived features to the dataset.")
+    ConvertoPseudorapidity(SelectedDirectory)        
+    print("Combinig combining all sub events in the events dataset.")    
+    recombineEvents(SelectedDirectory)
+        
---
+++ Feature_Plots_PCA.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 11:32:29 2020
+
+@author: gerha
+"""
+
+import numpy as np
+from random import randint
+import matplotlib.pyplot as plt
+import xgboost as xgb
+import pandas as pd 
+import seaborn as sns 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+
+def get_text_positions(x_data, y_data, txt_width, txt_height):
+    a = list(zip(y_data, x_data))
+    text_positions = y_data.copy()
+    for index, (y, x) in enumerate(a):
+        local_text_positions = [i for i in a if i[0] > (y - txt_height)
+                                and (abs(i[1]-x) < txt_width * 2) and i != (y,x)]
+        if local_text_positions:
+            sorted_ltp = sorted(local_text_positions)
+            if abs(sorted_ltp[0][0] - y) < txt_height:
+                differ = np.diff(sorted_ltp, axis = 0)
+                a[index] = (sorted_ltp[-1][0] + txt_height, a[index][1])
+                text_positions[index] = sorted_ltp[-1][0] + txt_height
+                for k, (j, m) in enumerate(differ):
+                    if j > txt_height * 1.5:
+                        a[index] = (sorted_ltp[k][0] + txt_height, a[index][1])
+                        text_positions[index] = sorted_ltp[k][0] + txt_height
+                        break
+    return text_positions
+
+def text_plotter(x_data, y_data, text_positions, labels, axis,txt_width,txt_height):
+    i = 0
+    for x,y,t in zip(x_data, y_data, text_positions):
+        axis.text(x - .03, 1.02*t, labels[i],rotation=0, color='black', fontsize=13)
+        i = i + 1
+        if y != t:
+            axis.arrow(x, t,0,y-t, color='black',alpha=0.2, width=txt_width*0.1,
+                       head_width=.02, head_length=txt_height*0.5,
+                       zorder=0,length_includes_head=True)
+
+def PLTColumns(column1, column2):
+  XData = []
+  YData = []
+  for i in range(dtrain.shape[0]):
+      if dtrain[i,column1] != -999 and dtrain[i,column2] != -999:
+         XData.append(dtrain[i,column1]) 
+         YData.append(dtrain[i,column2])
+  fig, ax = plt.subplots()
+  fig = plt.scatter(XData,YData);
+  ax.set_xlabel(xlabel = columnNames[column1])
+  ax.set_ylabel(ylabel = columnNames[column2])
+  plt.show()
+
+def FeaturePlots(DataSet):
+    sns.pairplot(DataSet.loc[:,DataSet.dtypes == 'float64'])
+    corr = DataSet.loc[:, DataSet.dtypes == 'float64'].corr()
+    plt.show()
+    sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, cmap=sns.diverging_palette(220, 10, as_cmap=True))
+
+def PCAAnalysis(DataSet, LabelOfInterest):
+    DataSet2 = DataSet.drop(labels = LabelOfInterest, axis = 1)
+    print(DataSet2.describe())
+    for col in np.unique(DataSet.columns):
+        plt.figure(num =None, figsize = [20, 20])
+        boxplot1 = DataSet.boxplot(by = "Label_Unique_Mother_Particles", column = col)
+        plt.show()
+    scalar = StandardScaler()
+    scalar.fit(DataSet2)
+    scaled_data =scalar.transform(DataSet2)
+    pca = PCA(n_components = 2)
+    pca.fit(scaled_data)
+    x_pca = pca.transform(scaled_data)
+    plt.figure(num =None, figsize = [20, 20])   
+    for g in tqdm(np.unique(DataSet[LabelOfInterest])):
+        i = np.where(np.abs(DataSet[LabelOfInterest]) == g)
+        plt.scatter(x_pca[i,0], x_pca[i,1], c = '#%06X' % randint(0, 0xFFFFFF), label = g )
+    plt.legend()
+    plt.xlabel('First Principal Component')
+    plt.ylabel('Second Principal Component')
+    plt.show()      
+    updatedColumns = list(DataSet2.columns)
+    df_comp= pd.DataFrame(pca.components_,columns = updatedColumns)
+    fig, ax = plt.subplots(figsize = (20, 20))
+    ax.set_xlim([-1,1])
+    ax.set_ylim([-1,1])
+    ax.set_xlabel('')
+    ax.scatter(pca.components_[0,:],pca.components_[1,:])
+    Circ = plt.Circle([0,0], radius = 1, fill = None)
+    ax.add_patch(Circ)
+    plt.xlabel('First Principal Component')
+    plt.ylabel('Second Principal Component')
+    ax.grid()
+    txt_height = 0.04*(plt.ylim()[1] - plt.ylim()[0])
+    txt_width = 0.04*(plt.xlim()[1]- plt.xlim()[0])
+    text_positions = get_text_positions(pca.components_[0,:],pca.components_[1,:],txt_width,txt_height)
+    text_plotter(pca.components_[0,:], pca.components_[1,:], text_positions,updatedColumns , ax , txt_width,txt_height)
+
+def RunAnalysisForwJetDataBase():
+    DataSet = pd.read_csv(r"C:\Users\gerha\Google Drive\Research\Post Doc\Physics\ML TTBar\wJetPsuedoRapidityDataSet.csv")
+    DataSet = DataSet.replace(to_replace = [np.inf, -np.inf], value = np.nan)
+    DataSet = DataSet.dropna()
+    DataSet = DataSet[DataSet.IST == 1]
+    print(DataSet)
+    DataSet = DataSet.drop(columns = ["EventID", "IST", "MOTH1", "MOTH2", "VTIM", "SPIN", "ICOL1", "ICOL2"], axis = 1)
+    print(DataSet.head())
+    DataSet = DataSet.drop(columns = "P3", axis = 1)
+    print(DataSet.head())
+    FeaturePlots(DataSet)
+    PCAAnalysis(DataSet, "PGD")
+    
+def RunAnalysisForTauDataBase():
+    DataSet = pd.read_csv(r"C:\Users\gerha\Google Drive\Research\Post Doc\Physics\Tau DataSet\PsuedoRapidityDataSet.csv")
+    DataSet = DataSet.replace(to_replace = [np.inf, -np.inf], value = np.nan)
+    DataSet = DataSet.dropna()
+    DataSet = DataSet[DataSet.IST == 1]
+    print(DataSet)
+    DataSet = DataSet.drop(columns = ["EventID", "IST", "MOTH1", "MOTH2", "VTIM", "SPIN", "ICOL1", "ICOL2"], axis = 1)
+    print(DataSet.head())
+    DataSet = DataSet.drop(columns = "P3", axis = 1)
+    print(DataSet.head())
+    FeaturePlots(DataSet)
+    PCAAnalysis(DataSet, "PGD")    
+    
